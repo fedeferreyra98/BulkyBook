56ce2ddd56712f8613be02f44df80c08cac1093b
Usuario <Usuario@DESARROLLO-NO-A>
2022-08-31 13:42:29 -0300

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]  //Data Annotation to set SQL attributes
+        public int Id { get; set; }
+        [Required] //This means the name cannot be null
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now; // This will assign that value after any new instance of Category.
+
+
+
+    }
+}
