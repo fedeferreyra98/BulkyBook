e1ba4492473ab39dd05a9f4d0a3d5e413509fd3f
Federico <feferreyra98@gmail.com>
2022-09-04 19:04:44 -0300

    Add Iterable of Category to controller and show it in html

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db; // I think the underscore before db is because is read only?
+
+        public CategoryController(ApplicationDbContext db) // Added this so i can use dependency injection
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList); // This way i pass the object to the view so i can show it in html
         }
     }
 }
