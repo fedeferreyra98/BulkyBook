a43e90c570c35df5a5fd91da09ec219ae5393771
Usuario <Usuario@DESARROLLO-NO-A>
2022-09-05 13:56:39 -0300

    Add constraints to DisplayOrder property in Category Model

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -8,6 +9,8 @@ namespace BulkyBookWeb.Models
         public int Id { get; set; }
         [Required] //This means the name cannot be null
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be between 1 and 100 only!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now; // This will assign that value after any new instance of Category.
 
